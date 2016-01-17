53284adbb34a2dd660cd5109dccc191e27b8ae02
Amanda Waliszewska <amy.wal@hotmail.com>
2016-01-17 03:23:22 +0100

    added validate textarea

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-/**
- * Created by Hakierka on 17.01.2016.
- */
+$(document).ready(function() {
+    function validateForm() {
+        //Form Validate
+        var inputText = $("#inputText");
+
+        inputText.on("input", function(event){
+
+            if($.isNumeric($(this).prop("value")) || $(this).prop("value").length <= 5){
+                $("#warning").removeClass("hidden-text").addClass("hidden-text-show");
+            }
+            else {
+                $("#warning").removeClass("hidden-text-show").addClass("hidden-text");
+            }
+
+        });
+
+    }
+
+    validateForm();
+
+});
